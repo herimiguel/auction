d176b2c122a130b571afeab725a1df33613da44e
Heriberto Miguel <herimiguel84@hotmail.com>
2018-06-26 09:01:43 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,202 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using auction.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+
+namespace auction.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private AuctionContext _context;
+
+        public HomeController(AuctionContext context)
+        {
+            _context = context;
+        }
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("")]
+        public IActionResult Index(UserModelView model)
+        {
+            if(ModelState.IsValid){
+                User newUser = new User {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Wallet = 1000,
+                    Username = model.Username,
+                    CreatedAt = DateTime.Now
+                };
+                PasswordHasher<User> hasher = new PasswordHasher<User>();
+                newUser.Password = hasher.HashPassword(newUser, model.Password);
+                _context.Add(newUser);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("currentUserId", newUser.Id);
+                    HttpContext.Session.SetString("currentUserName", newUser.FirstName);
+                return RedirectToAction("Dashboard");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult Login(){
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/login")]
+        public IActionResult Login(string username, string password){
+            User loginuser = _context.Users.SingleOrDefault(user => user.Username == username);
+            if(loginuser == null){
+                ViewBag.UsernameError = "Invalid Username! Typo?";
+                return View();
+            } else{
+                PasswordHasher<User> hasher = new PasswordHasher<User>();
+                if(hasher.VerifyHashedPassword(loginuser, loginuser.Password, password) != 0){
+                    HttpContext.Session.SetInt32("currentUserId", loginuser.Id);
+                    HttpContext.Session.SetString("currentUserName", loginuser.FirstName);
+                    return RedirectToAction("Dashboard");
+                } else {
+                    ViewBag.PasswordError = "Invlaid Password! Typo?";
+                    return View();
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("/dashboard")]
+        public IActionResult Dashboard(){
+            int? userId = HttpContext.Session.GetInt32("currentUserId");
+            User currentUser = _context.Users.SingleOrDefault(u => u.Id == (int)userId);
+
+            DateTime now = DateTime.Now;
+            List<Auction> allAuctions = _context.Auctions.OrderBy(a => a.EndDate).Where(auc => auc.EndDate > now).ToList();
+            List<Auction> expiredAuctions = _context.Auctions.Where(a => a.EndDate <= now).ToList();
+            foreach(var auction in expiredAuctions){
+                User highestBidder = _context.Users.SingleOrDefault(u => u.FirstName == auction.HighestBidder);
+                User createdUser = _context.Users.SingleOrDefault(u => u.FirstName + " " + u.LastName == auction.CreatedUser);
+                highestBidder.Wallet -= auction.Bid;
+                createdUser.Wallet += auction.Bid;
+            }
+            _context.SaveChanges();
+
+            Dictionary<int, int> remainingTime = new Dictionary<int, int>();
+            foreach(var auction in allAuctions){
+                remainingTime[auction.Id] = (auction.EndDate - now).Days;
+            }
+            
+            ViewBag.User = currentUser;
+            ViewBag.Auctions = allAuctions;
+            ViewBag.Reamining = remainingTime;
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/createauction")]
+        public IActionResult CreateAuction(){
+            int? userId = HttpContext.Session.GetInt32("currentUserId");
+            User currentUser = _context.Users.SingleOrDefault(u => u.Id == (int)userId);
+            ViewBag.User = currentUser;
+            return View();
+        }
+        [HttpPost]
+        [Route("/createauction")]
+        public IActionResult CreateAuction(AuctionViewModel model){
+            int? userId = HttpContext.Session.GetInt32("currentUserId");
+            User currentUser = _context.Users.SingleOrDefault(u => u.Id == (int)userId);
+            if(ModelState.IsValid){
+                Auction newAuction = new Auction {
+                    ProductName = model.ProductName,
+                    CreatedUser = currentUser.FirstName + " " + currentUser.LastName,
+                    Description = model.Description,
+                    Bid = model.Bid,
+                    EndDate = DateTime.Parse(model.EndDate),
+                    CreatedAt = DateTime.Now
+                };
+                _context.Add(newAuction);
+                _context.SaveChanges();
+                return RedirectToAction("Dashboard");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/auction/{id}")]
+        public IActionResult ShowAuction(string id){
+            int? userId = HttpContext.Session.GetInt32("currentUserId");
+            User currentUser = _context.Users.SingleOrDefault(u => u.Id == (int)userId);
+            int auctionId = Int32.Parse(id);
+            Auction showingAuction = _context.Auctions.SingleOrDefault(a => a.Id == auctionId);
+            ViewBag.Auction = showingAuction;
+            ViewBag.User = currentUser;
+            return View();
+        }
+        [HttpPost]
+        [Route("/auction/{id}")]
+        public IActionResult ShowAuction(string id, string bid){
+
+            List<string> errors = new List<string>();
+            int auctionId = Int32.Parse(id);
+            Auction showingAuction = _context.Auctions.SingleOrDefault(a => a.Id == auctionId);
+            if(bid != null){
+               
+                int bidAmount = Int32.Parse(bid);
+
+
+                int? userId = HttpContext.Session.GetInt32("currentUserId");
+                User currentUser = _context.Users.SingleOrDefault(u => u.Id == (int)userId);
+
+                if(bidAmount <= showingAuction.Bid){
+                    errors.Add("Has to be more than current bid");
+                    ViewBag.AmountErrors = errors;
+                    ViewBag.Auction = showingAuction;
+                    return View();
+                } else if(bidAmount > currentUser.Wallet) {
+                    errors.Add("You don't even have money");
+                    ViewBag.AmountErrors = errors;
+                    ViewBag.Auction = showingAuction;
+                    return View();
+                } else {
+                    showingAuction.Bid = bidAmount;
+                    showingAuction.HighestBidder = currentUser.FirstName;
+                    currentUser.Wallet -= bidAmount;
+                    _context.SaveChanges();
+                    return RedirectToAction("ShowAuction");
+                }
+            } else {
+                errors.Add("Did you forget?");
+                ViewBag.AmountErrors = errors;
+                ViewBag.Auction = showingAuction;
+                return View();
+            }
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public IActionResult Logout(){
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("/auction/delete/{id}")]
+        public IActionResult DeleteAuction(string id){
+            int auctionId = Int32.Parse(id);
+            Auction deletedAuction = _context.Auctions.SingleOrDefault(a => a.Id == auctionId);
+            _context.Auctions.Remove(deletedAuction);
+            _context.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
+    }
+}
